87efc2dbd87ac8edc227525a2afbdb051f7cf143
BengalDroid <bengaldroid@gmail.com>
2021-09-04 21:54:17 +0600

    pure function

---
+++ function_1.js
@@ -0,0 +1,25 @@
+// function sqr(n){
+    // return n*n
+// }
+// console.log(sqr(2))
+
+// var n =10
+// function change(n){
+    // var n=100
+    // console.log(n)
+// }
+// change()
+// console.log(n)
+
+var point={
+    x:45,
+    y:30
+}
+function printPoint(point){
+    point.x=100
+    point.y=200
+    console.log(point)
+
+}
+printPoint(point)
+console.log(point)
---
+++ opp.js
@@ -0,0 +1,10 @@
+var width =10
+var height =20
+function calculateArea(width, height){
+    return width*height
+}
+function calculateRange(width, height){
+    return 2*(width+height)
+}
+var area =calculateArea(width, height)
+var range =calculateRange(width, height)
